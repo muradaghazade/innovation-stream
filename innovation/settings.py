21e4a0162e9c35c1fde1d3a68a38e771b3a3efaa
Murad Aghazada <tariyelagazade@Murads-MacBook-Pro3.local>
2023-03-09 19:12:43 +0400

    m

--- innovation/settings.py
+++ innovation/settings.py
@@ -84,26 +84,26 @@ WSGI_APPLICATION = 'innovation.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-import time
-time.sleep(2)
-DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.getenv('POSTGRES_DB', 'taskilled'),
-            'USER': os.getenv('POSTGRES_USER', 'taskilled'),
-            'PORT': os.getenv('POSTGRES_PORT', 5432),
-            'PASSWORD': os.getenv('POSTGRES_PASSWORD', '12345'),
-            'HOST': os.getenv('POSTGRES_HOST', '127.0.0.1'),
-        }
-    }
-
-
+# import time
+# time.sleep(2)
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql',
+#             'NAME': os.getenv('POSTGRES_DB', 'taskilled'),
+#             'USER': os.getenv('POSTGRES_USER', 'taskilled'),
+#             'PORT': os.getenv('POSTGRES_PORT', 5432),
+#             'PASSWORD': os.getenv('POSTGRES_PASSWORD', '12345'),
+#             'HOST': os.getenv('POSTGRES_HOST', '127.0.0.1'),
+#         }
 #     }
-# }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
