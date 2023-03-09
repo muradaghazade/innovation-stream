c0aad58d379023187a71b59c08d1cbc4d236cdb5
Murad Aghazada <tariyelagazade@Murads-MacBook-Pro3.local>
2023-03-09 19:18:28 +0400

    m

--- innovation/settings.py
+++ innovation/settings.py
@@ -142,11 +142,13 @@ USE_TZ = True
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static',
+# ]
+
+STATIC_ROOT= os.path.join(BASE_DIR,'static_media/')
 
 MEDIA_URL = '/media/'
 
