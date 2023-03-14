71a1cf6da6b7d600647ac6a93661c641507b1020
Murad Aghazada <tariyelagazade@Murads-MacBook-Pro3.local>
2023-03-14 22:53:28 +0400

    m

--- core/admin.py
+++ core/admin.py
@@ -3,9 +3,15 @@ from django.contrib.auth.models import Group, User
 from core.models import *
 from ckeditor.widgets import CKEditorWidget
 
-admin.site.register(News)
+# admin.site.register(News)
 admin.site.register(Category)
 admin.site.register(Tag)
 admin.site.register(Subscriber)
 
 admin.site.unregister([Group, User])
+
+class NewsAdmin(admin.ModelAdmin):
+    class Media:
+        js = ('ckeditor/ckeditor/ckeditor.js','ckeditor/ckeditor/ckeditor-init.js',)
+
+admin.site.register(News, NewsAdmin)
