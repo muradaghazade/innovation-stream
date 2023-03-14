3a7cae031490b7b34e891709e8f0f41051bd27c9
Murad Aghazada <tariyelagazade@Murads-MacBook-Pro3.local>
2023-03-14 23:03:35 +0400

    m

--- core/admin.py
+++ core/admin.py
@@ -12,6 +12,6 @@ admin.site.unregister([Group, User])
 
 class NewsAdmin(admin.ModelAdmin):
     class Media:
-        js = ('ckeditor/ckeditor-init.js','ckeditor/ckeditor/ckeditor.js',)
+        js = ('ckeditor/ckeditor-init.js','ckeditor/ckeditor/ckeditor.js','ckeditor/ckeditor/styles.js','ckeditor/ckeditor/config.js',)
 
 admin.site.register(News, NewsAdmin)
