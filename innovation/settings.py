577247c5e10fbe587ce920b0420e1f197d39fea6
Murad Aghazada <tariyelagazade@Murads-MacBook-Pro3.local>
2023-03-14 23:40:16 +0400

    m

--- innovation/settings.py
+++ innovation/settings.py
@@ -176,7 +176,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # JET_INDEX_DASHBOARD = 'dashboard.CustomIndexDashboard'
 
-BROKER_URL = "redis://redis:6379"
+CELERY_BROKER_URL = "redis://redis:6379"
 CELERY_RESULT_BACKEND = "redis://redis:6379"
 CELERY_ACCEPT_CONTENT = ["application/json"]
 CELERY_TASK_SERIALIZER = "json"
