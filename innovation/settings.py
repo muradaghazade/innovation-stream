fcff5cd99bb32d128dca336b75fe89f2c91b6b0c
Murad Aghazada <tariyelagazade@Murads-MacBook-Pro3.local>
2023-04-03 01:52:28 +0400

    m

--- innovation/settings.py
+++ innovation/settings.py
@@ -30,7 +30,7 @@ PROD = not DEBUG
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['https://pashainnovation.com/']
+# CSRF_TRUSTED_ORIGINS = ['https://pashainnovation.com/']
 
 
 # Application definition
@@ -58,7 +58,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
